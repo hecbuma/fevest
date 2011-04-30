a7dff998fd42de6f3e78c6c681f5a986b2a8dc59
Gabriel Paladino <gabriel@scupen.com>
2011-04-30 17:21:10 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" dir="ltr" xml:lang="pt-br" lang="pt-br">
+    <head profile="http://gmpg.org/xfn/11">
+        
+        <title>FEVEST 2011 - EM BREVE</title>
+        
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="pt-br" />
+        <meta name="author" content="VibeClick Agência Interativa" />
+
+        <style type="text/css">
+        body {background: url(curva-top-esquerda.png) top left no-repeat; text-align:center; margin-top:100px;}
+        </style>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-1890117-34']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+    </head>
+    <body>
+
+        <img src="temporario.png">
+
+    </body>
+</html>
